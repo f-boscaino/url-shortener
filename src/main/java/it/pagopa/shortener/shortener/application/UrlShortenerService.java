50fb95ab5057e18ed9d84ed8bf9285c3b4b80a12
Francesco Boscaino <f.boscaino@gmail.com>
2024-06-06 01:21:32 +0200

    Add logs and k8s config

--- src/main/java/it/pagopa/shortener/shortener/application/UrlShortenerService.java
+++ src/main/java/it/pagopa/shortener/shortener/application/UrlShortenerService.java
@@ -4,6 +4,8 @@ import it.pagopa.shortener.shortener.domain.UrlMapping;
 import it.pagopa.shortener.shortener.domain.UrlMappingRepository;
 import it.pagopa.shortener.shortener.domain.UrlShortenerServicePort;
 import org.apache.commons.validator.ValidatorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,7 +13,9 @@ import java.util.Optional;
 @Service
 public class UrlShortenerService implements UrlShortenerServicePort {
 
-    private UrlMappingRepository urlMappingRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UrlShortenerService.class);
+
+    private final UrlMappingRepository urlMappingRepository;
 
     public UrlShortenerService(UrlMappingRepository urlMappingRepository) {
         this.urlMappingRepository = urlMappingRepository;
@@ -23,6 +27,7 @@ public class UrlShortenerService implements UrlShortenerServicePort {
 
     public UrlMapping save(UrlMapping urlMapping) throws ValidatorException {
         urlMapping = getUniqueUrlMapping(urlMapping);
+        logger.info(urlMapping.getLongUrl() + " mapped to: " + urlMapping.getShortUrl());
         return urlMappingRepository.save(urlMapping);
     }
 
@@ -35,6 +40,7 @@ public class UrlShortenerService implements UrlShortenerServicePort {
         UrlMapping currentUrlMapping = urlMapping.clone();
 
         while (getByShortUrl(currentUrlMapping.getShortUrl()).isPresent()) {
+            logger.debug(urlMapping.getShortUrl() + " already exists! Generating a new one");
             currentUrlMapping = new UrlMapping(currentUrlMapping.getLongUrl());
         }
 
